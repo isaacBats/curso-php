3c5d00f9bc8a63d9c582e3e03cab8e719f982842
Isaac Batista <daniel.bautista@pengostores.com>
2019-08-06 16:24:55 -0500

    Views

---
+++ me/views/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>POO</title>
+    <link rel="stylesheet" href="../assets/css/app.css">
+</head>
+<body>
+    <div class="app">
+        
+    </div>
+    <footer>
+        <script src="../assets/js/app.js"></script>
+    </footer> 
+</body>
+</html>
