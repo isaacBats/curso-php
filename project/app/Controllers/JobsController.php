6dd5e432696d7c9c98b7be5d35295cc055329346
Isaac Batista <daniel.bautista@pengostores.com>
2019-08-23 09:04:28 -0500

    add validations

--- project/app/Controllers/JobsController.php
+++ project/app/Controllers/JobsController.php
@@ -2,18 +2,30 @@
 
 namespace App\Controllers;
 
+use Respect\Validation\Validator;
+
 class JobsController extends BaseController
 {
     public function getAddJobAction ( $request ) {
+        $responseMessage = '';
+
         if ( $request->getMethod() == 'POST' ) {
             $postData = $request->getParsedBody();
-            $job = new \App\Models\Job();
-            $job->title = $postData['title'];
-            $job->description = $postData['description'];
-            $job->save();
+            $jobValidator = Validator::key('title', Validator::stringType()->notEmpty())
+                ->key('description', Validator::stringType()->notEmpty());
+            try {
+                $jobValidator->assert($postData);
+                $job = new \App\Models\Job();
+                $job->title = $postData['title'];
+                $job->description = $postData['description'];
+                $job->save();
+                $responseMessage = 'Saved';
+            } catch (\Exception $e) {
+                $responseMessage =  "Error: {$e->getMessage()}";
+            }
         }
 
-        return $this->renderHTML('addJob.twig');
+        return $this->renderHTML('addJob.twig', compact('responseMessage'));
     }
 
 }
