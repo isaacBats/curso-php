697d1a795ed44f8c04ee7e71807014b4310934cc
Isaac Batista <daniel.bautista@pengostores.com>
2019-08-23 13:27:17 -0500

    Upload files and save in database

--- project/app/Controllers/JobsController.php
+++ project/app/Controllers/JobsController.php
@@ -10,14 +10,31 @@ class JobsController extends BaseController
         $responseMessage = '';
 
         if ( $request->getMethod() == 'POST' ) {
-            $postData = $request->getParsedBody();
             $jobValidator = Validator::key('title', Validator::stringType()->notEmpty())
                 ->key('description', Validator::stringType()->notEmpty());
             try {
+                $postData = $request->getParsedBody();
                 $jobValidator->assert($postData);
+                
+                $files = $request->getUploadedFiles();
+                $logo = $files['logo']; 
+
+                if ( $logo->getError() == UPLOAD_ERR_OK ) {
+                    $filename = $logo->getClientFilename();
+                    $explodeFilename = explode(".", $filename);
+                    if ( sizeof($explodeFilename) > 2 ) {
+                        $newName = sha1(implode("_", $explodeFilename) . '_' . uniqid()) . '.' . end($explodeFilename);
+                    } else {
+                        $newName = sha1($filename . '_' . uniqid()) . '.' . $explodeFilename[1];
+                    }
+                    $logo->moveTo("uploads/{$newName}");
+                }
+                
                 $job = new \App\Models\Job();
                 $job->title = $postData['title'];
                 $job->description = $postData['description'];
+                $job->img_name = isset($filename) ? $filename : null;
+                $job->img_path = isset($newName) ? "uploads/{$newName}" : null;
                 $job->save();
                 $responseMessage = 'Saved';
             } catch (\Exception $e) {
